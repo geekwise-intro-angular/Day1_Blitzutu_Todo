539b3349287863dbe3d4459c67cc3b139c560456
Blitzutu <Sky1460@comcast.net>
2016-04-05 20:30:14 -0700

    My First App - Todo

---
+++ Day 1/Day1.js
@@ -0,0 +1,46 @@
+// Angular Class - 4-5-2016
+// S. Goodwin
+// Notes
+
+/*
+MVC
+Model - The Database
+View - How the Information is Viewed
+Controller - How the database is conncected.
+
+============================================================================
+ view = data input = IE E-Mail Address.
+ Controller - Speaks to the Database to compare information.
+ Model = the Area in which the data is stored and is communicated with by the controller.
+
+Model is used more to store the data and hold the data.
+View is user input and is compared to the Model (Database) by the Controller.
+Controller is used to compare user information to the Model that is store. IE Tell the User that the E-Mail address is incorrect.
+
+Builtwith.com - shows that website are built with.
+*/
+
+/* Start of Angular Presintation
+Angular = Framework
+
+libary - You control the code, Add the Libary to the code.
+
+Framework = framkework controls the code
+the framework gas sets of standards
+
+SPA = Single Page Application
+
+Angular is a MV* AKA Model View Whatever / Model, View / View Model
+
+Angular & HTML
+
+Angular is added to the HTML
+Angular extends the HTML through directives
+
+We can make our own custom attriubutes
+
+Angular has its own custom attriubutes
+Angular assists with making its own custom attriubutes
+* Can be accessed with JavaScrpit & CSS.
+ Angular is not the App to build all Apps.
+*/
